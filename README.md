fa0cd51362cba902b9a7c84da4e86bccd8f49d8c
Med Hacen <hacen13037@gmail.com>
2022-03-10 01:43:55 +0300

    First version

---
+++ README.md
@@ -0,0 +1,19 @@
+#Plate Number Detection
+This script permits to the user to enter a path to an image and then detect
+the plate number if it is present. The detected plate number will be surrounded by
+a rectangle.
+
+### Examples
+![example1](Resources/result1.png)
+![example2](Resources/result2.png)
+
+### Running the script
+```commandline
+python detector.py
+```
+
+### Libraries used
+- OpenCV
+
+### *Author Name*
+[Mohamed El Hacen Habib](https://github.com/mohamedelhacen)
---
+++ detector.py
@@ -0,0 +1,47 @@
+import cv2
+
+plateNumberCascade = cv2.CascadeClassifier("Resources/haarcascade_russian_plate_number.xml")
+
+def detect(image):
+    imageCopy = image.copy()
+    imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    try:
+        plateNumbers = plateNumberCascade.detectMultiScale(imageGray, 1.1, 10)
+        for (x, y, w, h) in plateNumbers:
+            area = w*h
+            if area > 200:
+                cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 255), 2)
+                cv2.putText(image, "Plate Number", (x, y-5), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2)
+                roi = image[y:y+h, x:x+w]
+                cv2.imshow("ROI", roi)
+        cv2.imshow("Original", imageCopy)
+        cv2.imshow("Output", image)
+    except:
+        print("No Plate Numbers detected")
+
+def detector():
+    path = input("Enter path to an image or video: ")
+    if path.endswith(('.jpg', '.jpeg', '.png')):
+        image = cv2.imread(path)
+        detect(image)
+        cv2.waitKey()
+    elif path.endswith('.mp4'):
+        cap = cv2.VideoCapture(path)
+        while True:
+            _, image = cap.read()
+            detect(image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    elif path == '0':
+        cap = cv2.VideoCapture(0)
+        while True:
+            _, image = cap.read()
+            detect(image)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    else:
+        print("Invalid input")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    detector()
